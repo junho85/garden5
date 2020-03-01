51eaf02beba1d16182654b888f70b8425a08ccab
june.kim <june.kim@kakaocorp.com>
2020-03-01 15:37:32 +0900

    wsgi config

--- garden5/wsgi.py
+++ garden5/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'garden5.settings')
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
+
