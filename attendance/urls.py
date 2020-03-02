5262941cf28ad6a49cd82bec65a7de921c6f7514
june.kim <june.kim@kakaocorp.com>
2020-03-02 12:45:10 +0900

    fix url - gets -> api/gets

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 app_name = 'attendance'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('users/', views.users, name='users'),
-    path('users/<user>/', views.user, name='user'),
-    path('api/users/<user>/', views.user_api, name='user'),
+    path('', views.index, name='index'), # 출석부 첫화면
+    path('api/users/', views.users, name='users'), # 정원사들 리스트
+    path('api/gets', views.gets, name='get'), # 전체 출석부 조회. 리스트. 유저별.
     path('collect/', views.collect, name='collect'), # slack_messages 수집
     path('get/<date>', views.get, name='get'), # 특정일의 출석부 조회. 날짜기준
-    path('gets', views.gets, name='get'), # 전체 출석부 조회. 리스트. 유저별.
+
+    path('users/<user>/', views.user, name='user'), # 유저별 출석부 데이터 페이지
+    path('api/users/<user>/', views.user_api, name='user'), # 특정 유저의 출석 데이터
 ]
